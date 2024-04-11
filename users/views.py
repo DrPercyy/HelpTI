848d52cc2deea44a662b7f3818276087f7634251
DrPercyy <juliodriver3@gmail.com>
2024-04-11 11:44:25 -0300

    adding user features

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,7 +8,15 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redireciona para a página de login ou para onde desejar
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
 
 def admin(request):
     pass
